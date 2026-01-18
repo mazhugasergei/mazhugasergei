e6f8687d656628ed7505ebeb0b7c1986ee0e8227
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-01-18 21:40:55 +1000

    .

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -10,5 +10,5 @@ export const isDevelopment = NODE_ENV === "development"
 export const isProduction = NODE_ENV === "production"
 export const isTest = NODE_ENV === "test"
 
-export const GITHUB_TOKEN = process.env.TOKEN || ""
-export const GITHUB_USERNAME = process.env.USERNAME
+export const GITHUB_TOKEN = process.env.GITHUB_TOKEN || ""
+export const GITHUB_USERNAME = process.env.GITHUB_USERNAME || ""
