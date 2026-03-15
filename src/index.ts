0384621d12209283f560a20174e9aef673a1327f
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-03-15 19:30:57 +1000

    style: padding & offset

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { getAsciiArtData } from "@/utils/content"
 import { generateImageFromAscii } from "@/utils/image"
 
 async function main() {
-  const styledLines = await getAsciiArtData({ debug: false, infoOffset: 8 })
+  const styledLines = await getAsciiArtData({ debug: false, infoOffset: 6 })
   generateImageFromAscii(styledLines, "image.svg", {
     padding: {
-      top: 80,
-      bottom: 80,
-      left: 80,
-      right: 450,
+      top: 70,
+      bottom: 70,
+      left: 70,
+      right: 420,
     },
   })
 }
